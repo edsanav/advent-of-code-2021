412c799c6dd24866039484d8c663a29f4518004e
Eduardo Santamaria <edsanav@gmail.com>
2021-12-10 19:45:25 +0100

    Day 7

--- src/main/scala/exercises/day7.scala
+++ src/main/scala/exercises/day7.scala
@@ -5,6 +5,8 @@ import cats.effect.ExitCode
 import cats.effect.std.Console
 import cats.implicits.*
 
+import scala.annotation.tailrec
+
 object day7 {
 
   def median(positions:List[Int]):Int = {
@@ -17,13 +19,38 @@ object day7 {
       (up.last+down.head)/2
     }
   }
+  def factorial(n:Int):Int = {
+    @tailrec
+    def go(num:Int, accum:Int):Int = num match {
+      case 0 => accum
+      case x => go(x-1, accum+x)
+    }
+    go(n, 0)
+  }
 
-  def distances(positions:List[Int], center:Int):Int = positions.map(pos => math.abs(pos-center)).sum
+  def distances(positions:List[Int], center:Int, adjust:Int=>Int = x => x):Int = positions.map(pos => adjust(math.abs(pos-center))).sum
+
+  @tailrec
+  def findCenter(positions:List[Int], startingPos: Int, adjust:Int=>Int = x => x):Int ={
+    val initD = distances(positions, startingPos, adjust)
+    val lowerD = distances(positions, startingPos-1, adjust)
+    val upperD = distances(positions, startingPos+1, adjust)
+    if (lowerD > initD){
+      if (upperD > initD){
+        startingPos
+      }else{
+        findCenter(positions, startingPos+1, adjust)
+      }
+    } else{
+      findCenter(positions, startingPos-1, adjust)
+    }
+  }
 
   def solve[F[_] : Console : Applicative](input: String): F[ExitCode] =
     val positions = input.split("\n").head.split(",").map(_.toInt).toList
     val part1 = distances(positions, median(positions))
-    Console[F].println((part1, 0)) *> Applicative[F].pure(ExitCode.Success)
+    val part2 = distances(positions, findCenter(positions, median(positions), factorial), factorial)
+    Console[F].println((part1, part2)) *> Applicative[F].pure(ExitCode.Success)
 
 
 }
