3f8ae61193da55e8eeff61565717c92b42902a93
Eduardo Santamaria <edsanav@gmail.com>
2021-12-16 00:21:26 +0100

    Day 8

--- src/main/scala/exercises/day8.scala
+++ src/main/scala/exercises/day8.scala
@@ -9,29 +9,46 @@ import scala.annotation.tailrec
 
 object day8 {
 
-  case class Display(
-                    U:Option[Char],
-                    UL:Option[Char],
-                    UR:Option[Char],
-                    M:Option[Char],
-                    LL:Option[Char],
-                    LR:Option[Char],
-                    L:Option[Char]
-                    ){
-    val isDone:Boolean = List(U, UL, UR, M, LL, LR, L).flatten.length == 7
+  def getCode(signals:List[Set[Char]]):Map[Set[Char], Int] = {
+    val (defined, others) = signals.partition(x => List(7, 3,4,2).contains(x.size))
+    val definedByLength = defined.map(ltt => (ltt.size,ltt)).toMap
+    val (len5, len6) = others.partition(_.size == 5)
+    val initial:Map[Int, Set[Char]] = Map(
+      1 -> definedByLength(2),
+      7 -> definedByLength(3),
+      4 -> definedByLength(4),
+      8 -> definedByLength(7)
+    )
+    val U = initial(7) -- initial(1)
+    val six = len6.filter(s => (s & initial(1)).size == 1).head
+    val zero = len6.filter(s => s!=six && (s & initial(4)).size == 3).head
+    val nine = len6.filter(s => s!=six && s!=zero).head
+    val five = len5.filter(s => (six & s) == s).head
+    val two = len5.filter(s => s != five && (s & initial(1)).size == 1).head
+    val three = len5.filter(s => s!=five && s!=two).head
+    initial.map{(num, s) => (s, num)} ++ Map(
+      zero ->0,
+      five ->5,
+      six->6,
+      nine->9,
+      two->2,
+      three->3
+    )
   }
 
-  def parse(line:String):(List[String],List[String]) = {
+  def parse(line:String):(List[Set[Char]],List[Set[Char]]) = {
     line.split("\\|") match {
-      case Array(x,y) => (x.trim.split(" ").toList, y.trim.split(" ").toList)
+      case Array(x,y) => (x.trim.split(" ").toList.map(_.toSet), y.trim.split(" ").toList.map(_.toSet))
       case _ => (List(), List())
     }
   }
 
   def solve[F[_] : Console : Applicative](input: String): F[ExitCode] =
-    val lines = input.split("\n")
-    val part1 = lines.flatMap(ln => parse(ln)._2).count(i => List(7, 3,4,2).contains(i.length))
-    val part2 = 0
+    val lines = input.split("\n").to(LazyList).map(parse)
+    val part1 = lines.flatMap(ln => ln._2).count(i => List(7, 3,4,2).contains(i.size))
+    val part2 = lines
+      .map{(signals, output) => (getCode(signals), output)}
+      .map{(code, output)=> output.map(word => code(word).toString).mkString.toInt}.sum
     Console[F].println((part1, part2)) *> Applicative[F].pure(ExitCode.Success)
 
 
